7c4ec3610a1e1111dfaafa56845960f706c9740c
Leszek Barszcz <leszek.barszcz@accenture.com>
2020-03-06 14:59:03 +0100

    MVC sample pt 1

---
+++ mvcSample/mvcSample/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mvcSample
+//
+//  Created by Leszek Barszcz on 05/03/2020.
+//  Copyright © 2020 lpb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
