771a9a47c6b9c26a0f8177f3d345e729927d55c5
Eugene Hyrol <eugene.hyrol@accenture.com>
2020-03-19 20:24:37 +0100

    search filtering

--- mvcSample/mvcSample/Controller/Search/SearchViewController.swift
+++ mvcSample/mvcSample/Controller/Search/SearchViewController.swift
@@ -7,39 +7,66 @@
 //
 
 import UIKit
+import CoreLocation
 
 final class SearchViewController: UIViewController {
     typealias SearchItem = (boldText: String, lightText: String)
 
     private let locationCellIdentifier = "locationCell"
 
-    private var items = [SearchItem(boldText: "Your current location", lightText: ""),
-                         SearchItem(boldText: "Warsaw", lightText: "Poland"),
+    private var items = [SearchItem(boldText: "Warsaw", lightText: "Poland"),
                          SearchItem(boldText: "Gdynia", lightText: "Poland"),
                          SearchItem(boldText: "Milano", lightText: "Italy"),
                          SearchItem(boldText: "Viena", lightText: "Austia")]
+    private var filteredItems = [SearchItem]()
+    private var isFiltering = false
+    private var tapGestureRecogniser: UITapGestureRecognizer!
+    private lazy var locationManager: CLLocationManager = CLLocationManager()
 
     weak var tableView: UITableView!
+    var searchBar: UISearchBar!
 
     override func loadView() {
         super.loadView()
 
         let tableView = UITableView()
+        self.searchBar = UISearchBar()
+        self.tapGestureRecogniser = UITapGestureRecognizer()
         self.view.addSubview(tableView)
+        self.view.addSubview(searchBar)
         self.tableView = tableView
+        self.tableView.addGestureRecognizer(tapGestureRecogniser)
+    }
+    
+    @objc
+    func hideKeyboard() {
+        view.endEditing(true)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+        
+        tapGestureRecogniser.addTarget(self, action: #selector(hideKeyboard))
+        searchBar.delegate = self
         tableView.backgroundColor = .clear
         tableView.rowHeight = 62
         tableView.separatorStyle = .none
 
         tableView.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        
+        searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+         tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor).isActive = true
+        
         tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+       
         tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
 
         tableView.register(LocationTableViewCell.self, forCellReuseIdentifier: locationCellIdentifier)
@@ -60,14 +87,18 @@ final class SearchViewController: UIViewController {
 
 extension SearchViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return isFiltering ? filteredItems.count : items.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: locationCellIdentifier, for: indexPath) as? LocationTableViewCell else { return UITableViewCell() }
 
-        cell.model = items[indexPath.row]
-
+        if isFiltering {
+            cell.model = filteredItems[indexPath.row]
+        } else {
+            cell.model = items[indexPath.row]
+        }
+        
         var accessoryImageType = UIImage(systemName: "paperplane.fill")
         if indexPath.row != 0 {
             accessoryImageType = UIImage(named: "clock")
@@ -77,5 +108,48 @@ extension SearchViewController: UITableViewDataSource {
         imageView.tintColor = .white
         cell.accessoryView = imageView
         return cell
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        isFiltering = true
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        isFiltering = false
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            hideKeyboard()
+        } else {
+            filteredItems = items.filter({ $0.boldText.contains(searchText) })
+        }
+        tableView.reloadData()
+    }
+}
+
+extension SearchViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status == .authorizedWhenInUse {
+            items.insert(SearchItem(boldText: "Your current location", lightText: ""), at: 0)
+            tableView.reloadData()
+        }
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations)
+        if items.count > 0,
+            let location = locations.first {
+            let newItem = SearchItem(boldText: "\(location.coordinate.latitude) \(location.coordinate.longitude)", lightText: "")
+            
+            items.remove(at: 0)
+            items.insert(newItem, at: 0)
+            tableView.reloadData()
+        }
     }
 }
